2c8370dc915e2c055259bc0fbafc724ac0682f00
uEduardo2124 <156321223+uEduardo2124@users.noreply.github.com>
2024-01-12 11:54:20 -0300

    v1.1

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from email.message import EmailMessage
 import smtplib
 import ssl
 
-meu_email = ''
+remetente_email = ''
 senha = ''
 destinatario_email = ''
 assunto = "Testando Python"
 body = '''
 Teste
 '''
-em = EmailMessage()
+email = EmailMessage()
 
-em['From'] = meu_email
-em['To'] = destinatario_email
-em['Subject'] = assunto
-em.set_content(body)
+email['From'] = remetente_email
+email['To'] = destinatario_email
+email['Subject'] = assunto
+email.set_content(body)
 
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(meu_email, senha)
-    smtp.sendmail(meu_email, destinatario_email, em.as_string())
+    smtp.sendmail(meu_email, destinatario_email, email.as_string())
